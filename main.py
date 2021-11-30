16213b25a3290f61af5c0c6b724f68f7fbb8a604
aimee-z <aimeez@umich.edu>
2021-11-29 19:32:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ import numpy as np
 import unittest
 import json
 import requests
-#hello
 
 ## Team Name: Women in Stem
 ## Team Members: Aimee Zheng, Sage Pei, Marina Sun
 
 
-# Create database
+# Create project database
 def setUpDatabase(db_name):
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db_name)
@@ -25,17 +24,28 @@ def covid_api():
     response_API = requests.get('https://api.covidtracking.com/v2/us/daily.json')
     data = response_API.text
     parse_json = json.loads(data)
-    # Check first date to make sure it works lol
-    # info = parse_json['data'][0]['date']
-    # print("\n\nFirst date included:\n", info)
     return parse_json
 
 # Compile COVID JS data into database
-# data here would be covid_api()
-def set_up_covid(data, cur, con):
-    setUpDatabase('project.db')
-    cur.execute("CREATE TABLE Covid (id INTEGER PRIMARY KEY, title TEXT)")
+def set_up_covid(data, cur, conn):
+    # is there something suspicious abt dropping table if exist? read project specs
+    cur.execute('DROP TABLE IF EXISTS "COVID Data"')
+    cur.execute('CREATE TABLE "COVID Data"("id" INTEGER PRIMARY KEY, "date" TEXT, "total_cases" INTEGER, "case_percent_population" REAL, "change_in_population" INTEGER, "hospitalized" INTEGER, "deaths" INTEGER)')
 
+    newdata = data['data']
+    count = 1
+    for i in newdata:
+        id = count
+        date = i['date']
+        # may need to trim down to 341 dates (4/2/20), too many dates for other missing data points
+        total_cases = i['cases']['total']['value']
+        case_percent_population = i['cases']['total']['calculated']['population_percent']
+        change_in_population = i['cases']['total']['calculated']['change_from_prior_day']
+        hospitalized = i['outcomes']['hospitalized']['currently']['value']
+        deaths = i['outcomes']['death']['total']['value']
+        cur.execute('INSERT INTO "Covid Data" (id, date, total_cases, case_percent_population, change_in_population, hospitalized, deaths) VALUES (?,?,?,?,?,?,?)', (id, date, total_cases, case_percent_population, change_in_population, hospitalized, deaths))
+        count = count + 1
+    conn.commit()
     pass
 
 # Get stock API data in JS:
@@ -57,7 +67,14 @@ def readDataFromFile(filename):
     return json_data
 
 def main():
-    set_up_covid()
+
+    cur, conn = setUpDatabase('project.db')
+
+    # Create COVID table
+    covid_data = covid_api()
+    set_up_covid(covid_data, cur, conn)
+
+    #
 
 if __name__ == "__main__":
     main()
