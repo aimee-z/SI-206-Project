322cfa849a0e5bd7830bfe01e9f3871a4e8c02b5
marina-sun <79865788+marina-sun@users.noreply.github.com>
2021-12-09 00:05:30 -0500

    REMOVED RANDOM CODE

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ def setUpDatabase(db_name):
     cur = conn.cursor()
     return cur, conn
 
-
-
 # Get National COVID-19 API data in JSON
 def covid_api():
     response_API = requests.get('https://api.covidtracking.com/v2/us/daily.json')
@@ -128,123 +126,6 @@ def set_up_bitcoin(data2, cur, conn, start):
     conn.commit()
     pass
 
-
-'''# Get stock API data in JSON:
-def stock_api():
-    api_access = '48e36d776c40e470ee12e76e5b9bd8cd'
-    stocks_url = 'https://api.polygon.io/v2/aggs/ticker/PFE/range/1/day/2020-01-13/2021-03-07?adjusted=true&sort=asc&limit=120&apiKey=zU1RScZjXgXk3X91fSvGZ8j5gNCUS4xy'
-    api3_result = requests.get('https://api.polygon.io/v2/aggs/ticker/PFE/range/1/day/2020-01-13/2021-03-07?adjusted=true&sort=asc&apiKey=zU1RScZjXgXk3X91fSvGZ8j5gNCUS4xy')
-    #data3 = json.loads(api3_result)
-    data3 = api3_result.json()
-    #print(data3)
-    return data3
-
-# Create stocks table 
-def stocks_table(data3, cur, conn):
-    #stocks_dict = stock_api()
-    newdata3 = data3
-
-    
-    cur.execute('DROP TABLE IF EXISTS "Stocks_Data"')
-    cur.execute('CREATE TABLE "Stocks_Data"("date_id" INTEGER PRIMARY KEY, "Date" TEXT, "highest_price" INTEGER, "lowest_price" INTEGER,"trading_volume" INTEGER, "transaction_number" INTEGER)')
-    key = 1 
-    
-    
-    while key < 26: 
-        for i in newdata3['results']:
-            if key < 26:
-                highest_price = i['h']
-                lowest_price = i['l']
-                trading_volume = i['v']
-                transaction_number = i['t']
-                #wrong parameter?? 
-                cur.execute('INSERT OR IGNORE INTO "Stocks_Data"(date_id,highest_price,lowest_price,trading_volume,transaction_number) VALUES(?,?,?,?,?)',(key,highest_price,lowest_price,trading_volume,transaction_number))
-                row_count = cur.rowcount 
-                if row_count > 0:
-                    key += 1 
-                    conn.commit()
-                else:
-                    break
-        continue
- 
-    #note: maybe try to delete data sets from within the time frame that matches id using a for loop & Select Delete Conditional after loading in data  ? 
-
-    #adding in dates list to stocks_data 
-    cur.execute("""SELECT Covid_Data.date FROM Covid_Data JOIN Stocks_Data ON Stocks_Data.date_id = Covid_Data.id;""")
-    date_list = cur.fetchall()
-    for i in range(1,len(date_list)+1):
-        cur.execute('UPDATE Stocks_Data set Date = ? where date_id = ? ',((date_list[i-1][0],i))) 
-        #cur.execute('INSERT INTO Stocks_Data(Date) VALUES(?)',((date_list[i-1][0],i)))
-        #cur.execute('INSERT INTO Stocks_Data.Date VALUES (?) WHERE date_id (?)', (date_list[i-1][0],i,))
-    conn.commit()
-
-    #print(newdata3)
-
-    #25 limiter 
-    cur.execute('SELECT * FROM Stocks_Data LIMIT 25')
-    selected_list = cur.fetchall()
-
-    #print(selected_list)
-
-    #for i in range(1,len(selected_list)):
-   #     cur.execute('DELETE Stocks_Data set Date = ? where date_id = ? ',((selected_list[i-1][0],i))) 
-   # conn.commit()'''
-
-
-
-
-#get iTunes Data
-'''def itunes_api():
-    #request for 100 itunes movie 
-    api1_result = requests.get('https://itunes.apple.com/search?term=movie&limit=100&entity=movie')
-    data2 = api1_result.json()
-    return data2 
-    
-    
-#create iTunes table
-def itunes_table(data, cur, conn):
-    cur.execute('DROP TABLE IF EXISTS "iTunes Data"')
-    cur.execute('CREATE TABLE "iTunes Data" ("Id" INTEGER PRIMARY KEY, "trackName" TEXT, "releaseDate" TEXT, "trackPrice" INTEGER, "primaryGenreName" TEXT, "contentAdvisoryRating" TEXT)')
-
-#compile iTunes data into database 
-def set_up_itunes(data, cur, conn):
-    count  = 1 
-    for i in data['results']:
-        Id = count
-        trackName = i['trackName']
-        releaseDate = i['releaseDate']
-        trackPrice = i['trackPrice']
-        primaryGenreName = i['primaryGenreName']
-        contentAdvisoryRating = i['contentAdvisoryRating']
-        cur.execute('INSERT INTO "iTunes Data"(Id,trackName,releaseDate,trackPrice,primaryGenreName,contentAdvisoryRating) VALUES (?,?,?,?,?,?)',(count,trackName,releaseDate,trackPrice,primaryGenreName,contentAdvisoryRating))
-        count = count + 1 
-    conn.commit()
-'''
-#get Currency Data: 
-'''def currency_api():
-    api3_result = requests.get('https://api.frankfurter.app/2020-01-03..2021-03-07')
-    data3 = api3_result.json()
-    return data3 
-
-#create Currency table: 
-def currency_table(data3,cur,conn):
-    cur.execute('CREATE TABLE IF NOT EXISTS "Currency Data"("Id" INTEGER PRIMARY KEY, "date" TEXT, "USD" INTEGER)')
-
-#compile Currency data into database 
-def set_up_currency(data3, cur,conn): 
-    newdata3 = data3
-
-    count3 = 1 
-    for i in newdata3['rates']:
-        id = count3
-        date = i[0]
-        USD = i['USD']
-        cur.execute('INSERT INTO "Currency Data"(Id,USD,date) VALUES (?,?,?)',(count3,USD,date))
-        count3 = count3 +1 
-    conn.commit()
-'''
-
-
 def main():
 
     cur, conn = setUpDatabase('project.db')
@@ -284,15 +165,6 @@ def main():
     else:
         for i in range(124, 149):
             set_up_ca_covid(ca_covid_data, cur, conn, i)
-
-    # Create stock table
-    '''Stocks_Data = stock_api()
-    stocks_table(Stocks_Data,cur,conn)
-    
-    set_up_stocks(Stocks_Data, cur, conn, '2020-01-13', '2020-02-06')
-    set_up_stocks(Stocks_Data, cur, conn,'2020-02-06', '2020-03-01')
-    set_up_stocks(Stocks_Data, cur, conn, '2020-10-01', '2020-10-25')
-    set_up_stocks(Stocks_Data, cur, conn, '2020-10-26', '2020-11-19')'''
     
     # Create Bitcoin Table
     bitcoin_data = bitcoin_api()
@@ -314,21 +186,6 @@ def main():
 
     # Join National and NY COVID Data
     calculations = join_tables(cur, conn)
-
-    #Create IMDB Table 
-    #IMDB_Data = IMDB_api()
-    #IMDB_table(IMDB_Data, cur, conn)
-
-    #Create iTunes table 
-    #itunes_data = itunes_api()
-    #itunes_table(itunes_data,cur,conn)
-    #set_up_itunes(itunes_data, cur, conn)
-
-    #Create Currency table 
-    #currency_data = currency_api()
-    #currency_table(currency_data,cur,conn)
-   # set_up_currency(currency_data,cur,conn)
-    
 
 if __name__ == "__main__":
     main()
